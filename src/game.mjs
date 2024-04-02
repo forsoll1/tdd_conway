c500e506b6b54af4619ac0be73f54800125fbd9a
Olli Forssell <olli20112@student.hamk.fi>
2024-04-02 18:30:22 +0300

    testing for tick mechanics

--- src/game.mjs
+++ src/game.mjs
@@ -1,16 +1,42 @@
+import { rleHandler } from "./rleHandler.mjs"
+
 export class game{
 
+    constructor(fileName = null){
+        if(fileName){
+            const rle = new rleHandler(fileName)
+            this.rules = rle.rules
+            this.coordinates = rle.coordinates
+        }else{
+            this.coordinates = []
+        }
+    }
+
+    tick(){
+        let newCoordinates = []
+        
+        for (const i of this.coordinates) {
+            let pointsAround = 0
+            let x = i[0]
+            let y = i[1]
+            let spacesAroundPoint = [[x-1,y-1],[x,y-1],[x+1,y-1],[x-1,y],[x+1,y],[x-1,y+1],[x,y+1],[x+1,y+1]]
+            for (const j of spacesAroundPoint) {
+                this.coordinates.findIndex(point => point[0] === j[0] && point[1] === j[1]) === -1 ? {} : pointsAround += 1
+            }
+            if (this.rules.S.includes(pointsAround)){
+                newCoordinates.push(i)
+            }
+        }
+        this.coordinates = newCoordinates
+    }
 
     patternFromCoordinates(coordinates){
-        let minY = coordinates.map((val => val[1])).reduce((a,b) => Math.min(a,b))
-        let minX = coordinates.map((val => val[0])).reduce((a,b) => Math.min(a,b))
-        let maxY = coordinates.map((val => val[1])).reduce((a,b) => Math.max(a,b))
-        let maxX = coordinates.map((val => val[0])).reduce((a,b) => Math.max(a,b))
+        let XY = this.getMinMax(coordinates)
         
         let patternChars = []
-        for (let y = minY; y < maxY + 1; y++) {
+        for (let y = XY.minY; y < XY.maxY + 1; y++) {
             let rowChars = ["$"]
-            for (let x = maxX; x > minX - 1; x--) {
+            for (let x = XY.maxX; x > XY.minX - 1; x--) {
                 if(coordinates.findIndex(point => point[0] === x && point[1] === y) !== -1){
                     rowChars.unshift("o")
                 }else if(rowChars.length > 1){
@@ -48,8 +74,20 @@ export class game{
     }
 
     ruleToString(parsedRule){
-        // "rule = B3/S23" {B:[3], S:[2,3]}
         let ruleString = `rule = B${parsedRule.B.join("")}/S${parsedRule.S.join("")}`
         return ruleString
+    }
+
+    XYToString(coordinates){
+        let XY = this.getMinMax(coordinates)
+        return `x = ${XY.maxX - XY.minX + 1}, y = ${XY.maxY - XY.minY + 1}`
+    }
+
+    getMinMax(coordinates){
+        let minY = coordinates.map((val => val[1])).reduce((a,b) => Math.min(a,b))
+        let minX = coordinates.map((val => val[0])).reduce((a,b) => Math.min(a,b))
+        let maxY = coordinates.map((val => val[1])).reduce((a,b) => Math.max(a,b))
+        let maxX = coordinates.map((val => val[0])).reduce((a,b) => Math.max(a,b))
+        return {minY: minY, minX: minX, maxY: maxY, maxX: maxX}
     }
 }
--- test/game.test.mjs
+++ test/game.test.mjs
@@ -4,21 +4,91 @@ import { rleHandler } from "../src/rleHandler.mjs"
 import { game } from "../src/game.mjs"
 const fs = require("fs")
 
-describe("Tests for Conway's Game of Life mechanics", () => {
+let filePath = "./patterns/foo.txt"
+let fileName = "foo.txt"
+
+function createFile(content){
+  try {
+    fs.writeFileSync(filePath, content)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+describe("Tests for printing out RLE pattern", () => {
 
     let newGame;
+    beforeEach(() => {
+        newGame = new game()
+    })
 
     test("Turn point coordinates back into RLE pattern line", () => {
         let coordinates = [[0,0],[1,0],[0,1],[1,1]]
-        newGame = new game()
         let result = newGame.patternFromCoordinates(coordinates)
         expect(result).to.equal("2o$2o!")
     })
 
     test("Return rule in as a string", () => {
         let parsedRule = {B:[3], S:[2,3]}
-        newGame = new game()
         let result = newGame.ruleToString(parsedRule)
         expect(result).to.equal("rule = B3/S23")
+    })
+
+    test("Return XY header string", () => {
+        let coordinates = [[0,0],[1,0],[0,1],[1,1]]
+        let result = newGame.XYToString(coordinates)
+        expect(result).to.equal("x = 2, y = 2")
+    })
+})
+
+describe("Tests for game mechanics", () => {
+
+    let newGame;
+
+    beforeAll(() => {
+        try {
+          fs.unlinkSync(filePath)
+        } catch (error) {}
+    })
+
+    beforeEach(() => {
+        newGame = new game()
+    })    
+    
+    afterEach(() => {
+        try {
+            fs.unlinkSync(filePath)
+        } catch (error) {}
+    })
+
+    test("Game gets rules and coordinates from RLE file", () => {
+        let msg = 
+`x = 1, y = 1, rule = B3/S23
+o!`
+        createFile(msg)
+        const testGame = new game(fileName)
+        expect(testGame.coordinates).to.deep.equal([[0,0]])
+        expect(testGame.rules).to.deep.equal({B:[3], S:[2,3]})
+    })
+
+    test("tick makes single point pattern disappear", () => {
+        newGame.rules = {B:[3], S:[2,3]}
+        newGame.coordinates = [[0,0]]
+        newGame.tick()
+        expect(newGame.coordinates).to.deep.equal([])
+    })
+
+    test("tick doesn't change 2x2 block", () => {
+        newGame.rules = {B:[3], S:[2,3]}
+        newGame.coordinates = [[0,0],[0,1],[1,0],[1,1]]
+        newGame.tick()
+        expect(newGame.coordinates).to.deep.equal([[0,0],[0,1],[1,0],[1,1]])
+    })
+
+    test("tick makes pattern disappear partially", () => {
+        newGame.rules = {B:[3], S:[2,3]}
+        newGame.coordinates = [[0,0],[1,1],[0,2]]
+        newGame.tick()
+        expect(newGame.coordinates).to.deep.equal([[1,1]])
     })
 })
