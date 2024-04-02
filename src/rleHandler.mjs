563862270475769165d6c3935994451ceb5238ac
Olli Forssell <olli20112@student.hamk.fi>
2024-04-02 13:47:33 +0300

    test passes

--- src/rleHandler.mjs
+++ src/rleHandler.mjs
@@ -11,4 +11,28 @@ export class rleHandler{
             return error
         }
     }
+
+    removeUselessLines(fileData){
+        fileData = fileData.replace(" ", "")
+        let rleLines = fileData.split("\n")
+        let counter = -1
+        for (let i = 0; i < rleLines.length; i++) {
+            if (rleLines[i][0] === "#"){counter = i}    
+        }
+        if(counter > -1){
+            rleLines.splice(0, counter + 1)
+        }
+        if(rleLines.length > 2){ rleLines.splice(2)
+        }
+        return rleLines
+    }
+
+    getRuleFromRLELine(line){
+        let b = line.match(/[Bb]\d+/)
+        let s = line.match(/[Ss]\d+/)
+        let bNums = b[0].match(/\d/g).map((val) => Number(val))
+        let sNums = s[0].match(/\d/g).map((val) => Number(val))
+        let result = {B: bNums, S: sNums}
+        return result
+    }
 }
--- test/rleHandling.test.mjs
+++ test/rleHandling.test.mjs
@@ -1,6 +1,7 @@
 import { describe, test, beforeEach, afterEach, beforeAll } from "vitest";
 import { expect } from "chai";
 import { rleHandler } from "../src/rleHandler.mjs"
+import exp from "constants";
 const fs = require("fs")
 
 let filePath = "./patterns/foo.txt"
@@ -42,16 +43,25 @@ describe("File reading", () => {
   });
 
   test("Return only useful lines of RLE file", () => {
-    let msg = `#N Glider
-    #O Richard K. Guy
-    #C The smallest, most common, and first discovered spaceship. Diagonal, has period 4 and speed c/4.
-    #C www.conwaylife.com/wiki/index.php?title=Glider
-    x = 3, y = 3, rule = B3/S23
-    bob$2bo$3o!`
+    let msg = 
+`#N Glider
+#O Richard K. Guy
+#C The smallest, most common, and first discovered spaceship. Diagonal, has period 4 and speed c/4.
+#C www.conwaylife.com/wiki/index.php?title=Glider
+x = 3, y = 3, rule = B3/S23
+bob$2bo$3o!
+IGNORELINE
+IGNORELINE2`
     createFile(msg)
     let data = rle.readFile(fileName)
     let result = rle.removeUselessLines(data)
-    expect(result).to.equal(["x = 3, y = 3, rule = B3/S23", "bob$2bo$3o!"])
+    expect(result).to.deep.equal(["x = 3, y = 3, rule = B3/S23", "bob$2bo$3o!"])
+  })
+
+  test("Return 'rules' of RLE file", () => {
+    let rleLines = ["x = 3, y = 3, rule = B3/S23", "bob$2bo$3o!"]
+    let result = rle.getRuleFromRLELine(rleLines[0])
+    expect(result).to.deep.equal({B:[3], S:[2,3]})
   })
 
 });
