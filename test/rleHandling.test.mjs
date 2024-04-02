e68cf698597b9e053fecc22f442f6ed732592657
Olli Forssell <olli20112@student.hamk.fi>
2024-04-02 13:13:10 +0300

    test for removing useless RLE lines

--- test/rleHandling.test.mjs
+++ test/rleHandling.test.mjs
@@ -1,26 +1,57 @@
-import { describe, test, beforeEach } from "vitest";
+import { describe, test, beforeEach, afterEach, beforeAll } from "vitest";
 import { expect } from "chai";
 import { rleHandler } from "../src/rleHandler.mjs"
 const fs = require("fs")
 
+let filePath = "./patterns/foo.txt"
+let fileName = "foo.txt"
 
-describe("File reading", () => {
-
-  let filePath = "./patterns/foo.txt"
-  let fileName = "foo.txt"
-  let msg = "Hello!"
-  let rle;
+function createFile(content){
   try {
-    fs.writeFileSync(filePath, msg)
+    fs.writeFileSync(filePath, content)
   } catch (error) {
     console.log(error)
   }
+}
+
+
+describe("File reading", () => {
+  let rle;
+
   beforeEach(()=>{
     rle = new rleHandler()
   })
 
+  beforeAll(() => {
+    try {
+      fs.unlinkSync(filePath)
+    } catch (error) {}
+  })
+
+  afterEach(() => {
+    try {
+      fs.unlinkSync(filePath)
+    } catch (error) {}
+  })
+
   test("Read file", () => {
+    let msg = "Hello!"
+    createFile(msg)
     let data = rle.readFile(fileName)
     expect(data).to.equal("Hello!")
   });
+
+  test("Return only useful lines of RLE file", () => {
+    let msg = `#N Glider
+    #O Richard K. Guy
+    #C The smallest, most common, and first discovered spaceship. Diagonal, has period 4 and speed c/4.
+    #C www.conwaylife.com/wiki/index.php?title=Glider
+    x = 3, y = 3, rule = B3/S23
+    bob$2bo$3o!`
+    createFile(msg)
+    let data = rle.readFile(fileName)
+    let result = rle.removeUselessLines(data)
+    expect(result).to.equal(["x = 3, y = 3, rule = B3/S23", "bob$2bo$3o!"])
+  })
+
 });
